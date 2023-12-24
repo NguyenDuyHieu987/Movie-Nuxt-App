1444f33c6d1ea6a541d6ed67abaea2415341b57a
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2023-12-24 22:26:27 +0700

    add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -78,6 +78,7 @@ export default defineNuxtConfig({
   },
   alias: {
     '@': resolve(__dirname, './')
+    // components: fileURLToPath(new URL('./components', import.meta.url))
   },
   imports: {
     dirs: [
