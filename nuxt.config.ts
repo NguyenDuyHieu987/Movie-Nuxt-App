79f34b56b674153e5faf16d0bb2e474bb1589579
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2023-12-30 21:12:55 +0700

    add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,6 +44,12 @@ export default defineNuxtConfig({
       ],
       script: [
         {
+          src: 'https://cdn-go.cn/aegis/aegis-sdk/latest/aegis.min.js?max_age=7776000',
+          // src: 'https://tam.cdn-go.cn/aegis-sdk/latest/aegis.min.js?max_age=7776000',
+          type: 'text/javascript',
+          async: true
+        },
+        {
           src: 'https://accounts.google.com/gsi/client',
           type: 'text/javascript',
           async: true
