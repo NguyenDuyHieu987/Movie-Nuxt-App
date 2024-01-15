373eb30e6fe52a81dd345c8a8f60b67985a1cf20
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2024-01-15 14:59:35 +0700

    chore: update

--- test/pages/auth.test.ts
+++ test/pages/auth.test.ts
@@ -2,7 +2,7 @@ import { mount } from '@vue/test-utils';
 import { describe, expect, it } from 'vitest';
 // import LoginPage from '../pages/login/index.vue';
 
-describe('Auhorization', () => {
+describe('Authorization', () => {
   it('Login Page', () => {
     // const result = mount(LoginPage);
     // expect(result.html()).toMatchSnapshot();
