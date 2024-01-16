96702d37f9d24cdb643f4d0bcf5a809f5a1a92fc
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2024-01-16 21:04:41 +0700

    chore: update

--- test/pages/auth.test.ts
+++ test/pages/auth.test.ts
@@ -1,5 +1,5 @@
 import { mount } from '@vue/test-utils';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, test } from 'vitest';
 // import LoginPage from '../pages/login/index.vue';
 
 describe('Authorization', () => {
