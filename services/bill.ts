68597cfe6bdeeade80d478642561f4c2dbdf8c26
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2024-03-24 09:07:08 +0700

    chore: update

--- services/bill.ts
+++ services/bill.ts
@@ -3,9 +3,5 @@ import { makeRequest } from './makeRequest';
 const PREFIX_ROUTE = 'bill';
 
 export function getBills(skip: number = 1, limit: number = 20) {
-  const authStore = useAuthStore();
-
-  if (!authStore.isLogin) return new Promise(() => {});
-
   return makeRequest(`/${PREFIX_ROUTE}/get-all?skip=${skip}&limit=${limit}`);
 }
