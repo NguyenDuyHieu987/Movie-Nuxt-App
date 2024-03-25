482b64342fd7560668b588d08f857003fe939f0e
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2024-03-25 07:40:32 +0700

    chore: update

--- components/Layouts/Header/SearchDropdown/SearchDropdown.vue
+++ components/Layouts/Header/SearchDropdown/SearchDropdown.vue
@@ -10,33 +10,33 @@
           v-if="dataSearch?.length && isShowSearchResults"
           class="search-results"
         >
-          <div class="search-results-list">
-            <div class="search-dropdown-header search-results">
-              <div class="left">Kết quả tìm kiếm</div>
-              <div class="right click-active">
-                <NuxtLink
-                  class="view-all"
-                  :to="`/search?q=${valueInput
-                    ?.replaceAll(' ', '+')
-                    .toLowerCase()}`"
-                  @click="isFocusSearchInput = false"
+          <div class="search-dropdown-header search-results">
+            <div class="left">Kết quả tìm kiếm</div>
+            <div class="right click-active">
+              <NuxtLink
+                class="view-all"
+                :to="`/search?q=${valueInput
+                  ?.replaceAll(' ', '+')
+                  .toLowerCase()}`"
+                @click="isFocusSearchInput = false"
+              >
+                Tất cả
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1.2rem"
+                  height="1.2rem"
+                  viewBox="0 0 320 512"
+                  fill="currentColor"
                 >
-                  Tất cả
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="1.2rem"
-                    height="1.2rem"
-                    viewBox="0 0 320 512"
-                    fill="currentColor"
-                  >
-                    <path
-                      d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256L73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"
-                    />
-                  </svg>
-                </NuxtLink>
-              </div>
+                  <path
+                    d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256L73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"
+                  />
+                </svg>
+              </NuxtLink>
             </div>
+          </div>
 
+          <div class="search-results-list">
             <div
               v-for="(item, index) in dataSearch"
               :key="index"
