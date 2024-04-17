2265aae7c181bb5cfa73b335e541d70b0579ef75
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2024-04-17 17:28:51 +0700

    chore: update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -295,7 +295,8 @@ export default defineNuxtConfig({
         'ant-design-vue/dist': 'ant-design-vue/dist',
         'ant-design-vue/es': 'ant-design-vue/es',
         'ant-design-vue/lib': 'ant-design-vue/es',
-        'ant-design-vue': 'ant-design-vue/es'
+        'ant-design-vue': 'ant-design-vue/es',
+        'element-plus/es': 'element-plus/es'
       }
     },
     plugins: [svgLoader({})],
