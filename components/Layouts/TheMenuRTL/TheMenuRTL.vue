8042f8314e5ca0eda24fe7771129761780bf7415
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2024-04-30 08:40:51 +0700

    chore: update

--- components/Layouts/TheMenuRTL/TheMenuRTL.vue
+++ components/Layouts/TheMenuRTL/TheMenuRTL.vue
@@ -3,7 +3,7 @@
     :collapse="openSiderBarFixed || props.noCollapse ? false : collapsed"
     :default-openeds="state.openKeys"
     :default-active="state.selectedKeys"
-    class="menu-sider-bar rtl"
+    class="menu-sider-bar-rtl"
     theme="dark"
     popper-effect="dark"
     background-color="transparent"
