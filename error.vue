08d0eff9c0096be0cb4c15cfe9e817455e1cfad0
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2024-05-05 09:20:28 +0700

    chore: update

--- error.vue
+++ error.vue
@@ -1,79 +1,81 @@
 <template>
   <div id="app">
-    <div class="app-wrapper error">
-      <NuxtLayout :name="isNetworkError ? 'error' : 'default'">
-        <a-result
-          class="error-page"
-          :status="isNetworkError ? 500 : error.statusCode"
-          :title="
-            isNetworkError
-              ? 'Kết nối mạng'
-              : ` ${
-                  utils.isStringEmpty(error.statusMessage)
-                    ? error.statusCode
-                    : error.statusMessage
-                }`
-          "
-        >
-          <template
-            v-if="error.statusCode == 404 && !isNetworkError"
-            #subTitle
+    <a-extract-style>
+      <div class="app-wrapper error">
+        <NuxtLayout :name="isNetworkError ? 'error' : 'default'">
+          <a-result
+            class="error-page"
+            :status="isNetworkError ? 500 : error.statusCode"
+            :title="
+              isNetworkError
+                ? 'Kết nối mạng'
+                : ` ${
+                    utils.isStringEmpty(error.statusMessage)
+                      ? error.statusCode
+                      : error.statusMessage
+                  }`
+            "
           >
-            <h3>Ops!, Không thể tìm thấy trang này.</h3>
-          </template>
-
-          <template
-            v-else-if="error.statusCode != 404 && !isNetworkError"
-            #subTitle
-          >
-            <h3>Ops!, It looks like something broke.</h3>
-          </template>
-
-          <template
-            v-else-if="isNetworkError"
-            #subTitle
-          >
-            <h3>Không có kết nối Internet. Vui lòng kiểm tra lại mạng.</h3>
-          </template>
-
-          <template #extra>
-            <a-button
-              v-if="!isNetworkError"
-              type="text"
-              size="large"
-              class="default"
-              @click="$router.back()"
+            <template
+              v-if="error.statusCode == 404 && !isNetworkError"
+              #subTitle
             >
-              Quay lại
-            </a-button>
+              <h3>Ops!, Không thể tìm thấy trang này.</h3>
+            </template>
 
-            <NuxtLink
-              v-if="!isNetworkError"
-              to="/"
-              @click.prevent="handleError"
+            <template
+              v-else-if="error.statusCode != 404 && !isNetworkError"
+              #subTitle
             >
+              <h3>Ops!, It looks like something broke.</h3>
+            </template>
+
+            <template
+              v-else-if="isNetworkError"
+              #subTitle
+            >
+              <h3>Không có kết nối Internet. Vui lòng kiểm tra lại mạng.</h3>
+            </template>
+
+            <template #extra>
               <a-button
+                v-if="!isNetworkError"
                 type="text"
                 size="large"
-                class="gradient"
+                class="default"
+                @click="$router.back()"
               >
-                Quay về trang chủ
+                Quay lại
               </a-button>
-            </NuxtLink>
 
-            <a-button
-              v-if="isNetworkError"
-              type="text"
-              size="large"
-              class="default"
-              @click="handleTryAgain"
-            >
-              Thử lại
-            </a-button>
-          </template>
-        </a-result>
-      </NuxtLayout>
-    </div>
+              <NuxtLink
+                v-if="!isNetworkError"
+                to="/"
+                @click.prevent="handleError"
+              >
+                <a-button
+                  type="text"
+                  size="large"
+                  class="gradient"
+                >
+                  Quay về trang chủ
+                </a-button>
+              </NuxtLink>
+
+              <a-button
+                v-if="isNetworkError"
+                type="text"
+                size="large"
+                class="default"
+                @click="handleTryAgain"
+              >
+                Thử lại
+              </a-button>
+            </template>
+          </a-result>
+        </NuxtLayout>
+      </div>
+    </a-extract-style>
   </div>
 </template>
 
