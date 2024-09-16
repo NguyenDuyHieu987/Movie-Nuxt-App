b73c1eb7d4865fe8fbd83fc8069d87b0bd8c2c78
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2024-09-16 19:24:37 +0700

    chore: update

--- components/VideoPlayer/VideoPlayer.vue
+++ components/VideoPlayer/VideoPlayer.vue
@@ -672,7 +672,6 @@
 </template>
 
 <script setup lang="ts">
-import './VideoPlayer.scss';
 import ReplayIcon from '~/assets/svgs/icons/replay.svg?component';
 import PlayIcon from '~/assets/svgs/icons/play.svg?component';
 import PauseIcon from '~/assets/svgs/icons/pause.svg?component';
@@ -1565,7 +1564,7 @@ const handleClickVideoPlayer = () => {
 };
 </script>
 
-<!-- <style lang="scss" src="./VideoPlayer.scss" scoped></style> -->
+<style lang="scss" src="./VideoPlayer.scss" scoped></style>
 <!-- <style lang="scss">
 @import url('./VideoPlayer.scss');
 </style> -->
