0167425da7dc852bf0c9ebe9e20adae17cba1f57
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2024-09-17 20:41:47 +0700

    chore: update

--- components/Layouts/Header/index.ts
+++ components/Layouts/Header/index.ts
@@ -1,6 +1,6 @@
-export { default as DropdownAccount } from './DropdownAccount/DropdownAccount.vue';
-export { default as Header } from './Header.vue';
-export { default as Notification } from './Notification/Notification.vue';
-export { default as SearchCard } from './SearchCard/SearchCard.vue';
-export { default as SearchDropdown } from './SearchDropdown/SearchDropdown.vue';
-export { default as SearchMobile } from './SearchMobile/SearchMobile.vue';
+// export { default as DropdownAccount } from './DropdownAccount/DropdownAccount.vue';
+// export { default as Header } from './Header.vue';
+// export { default as Notification } from './Notification/Notification.vue';
+// export { default as SearchCard } from './SearchCard/SearchCard.vue';
+// export { default as SearchDropdown } from './SearchDropdown/SearchDropdown.vue';
+// export { default as SearchMobile } from './SearchMobile/SearchMobile.vue';
