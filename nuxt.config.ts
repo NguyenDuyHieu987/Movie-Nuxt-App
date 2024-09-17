f7b2c823831a204569bf7d3fc635aeff44cbb577
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2024-09-17 10:38:10 +0700

    chore: update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -483,7 +483,7 @@ export default defineNuxtConfig({
       },
       // manifest: true,
       // ssrManifest: true,
-      sourcemap: 'hidden',
+      sourcemap: 'inline',
       // assetsDir: '_nuxt/',
       cssMinify: 'lightningcss',
       cssCodeSplit: false,
