9045c90bc0a9415e749d4a43b4f3cb622bc2c51f
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2024-09-28 06:58:15 +0700

    chore: update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -438,12 +438,12 @@ export default defineNuxtConfig({
 
   vite: {
     resolve: {
-      // alias: {
-      //   'ant-design-vue/dist': 'ant-design-vue/dist',
-      //   'ant-design-vue/es': 'ant-design-vue/es',
-      //   'ant-design-vue/lib': 'ant-design-vue/es',
-      //   'ant-design-vue': 'ant-design-vue/es'
-      // }
+      alias: {
+        'ant-design-vue/dist': 'ant-design-vue/dist',
+        'ant-design-vue/es': 'ant-design-vue/es',
+        'ant-design-vue/lib': 'ant-design-vue/es',
+        'ant-design-vue': 'ant-design-vue/es'
+      }
     },
     plugins: [
       // svgLoader({}),
