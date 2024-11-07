55b68cbfe16c37be3e684829bcf6129005b5aad7
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2024-11-07 23:11:29 +0700

    chore: update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -487,7 +487,7 @@ export default defineNuxtConfig({
       ssrManifest: true,
       sourcemap: 'hidden',
       // assetsDir: '_nuxt/',
-      cssMinify: 'lightningcss',
+      cssMinify: 'esbuild',
       cssCodeSplit: true,
       reportCompressedSize: true,
       // target: 'esnext',
