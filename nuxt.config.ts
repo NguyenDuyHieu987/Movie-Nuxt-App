f1149af2c101e0a7c684c18b7d5d9ca8630e417e
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2024-11-08 08:50:48 +0700

    chore: update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -177,7 +177,7 @@ export default defineNuxtConfig({
     '@nuxtjs/google-fonts',
     // 'nuxt-gtag',
     'nuxt-lodash',
-    'nuxt-delay-hydration',
+    // 'nuxt-delay-hydration',
     // 'nuxt-purgecss',
     // SEO
     '@nuxtjs/seo',
